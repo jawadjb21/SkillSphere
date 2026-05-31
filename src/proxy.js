559caa67249b7a18d1af8e0b91ada275af2b58f2
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-05-31 19:37:33 +0200

    build: proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,26 +1,19 @@
 import { NextResponse } from 'next/server'
-// import { auth } from './lib/auth';
+import { auth } from './lib/auth.js';
 import { headers } from 'next/headers';
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request) {
-    // const session = await auth.api.getSession({
-    //     headers: await headers()
-    // })
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
 
-    // console.log(session);
-    // // If logged in. let user access the requested url.
-    // if (session) {
-    //     return NextResponse.next();
-    // }
-    // // Else redirect to login page.
-    // return NextResponse.redirect(new URL('/login', request.url))
+    if (session) {
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/login', request.url))
 }
 
-// Alternatively, you can use a default export:
-// export default function proxy(request) { ... }
-
 export const config = {
-    // Mention the paths(static or dynamic) for proxy to operate on.
-    matcher: ["/"],
+    matcher: ["/", "/courses", "/courses/:path", "/profile"],
 }
