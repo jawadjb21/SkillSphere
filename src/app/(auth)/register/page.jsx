8d93c2eda2e2c1a63accc868f0961e4e89a210c3
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-05-31 10:09:21 +0200

    build: register interface

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Registerpage = () => {
+    return (
+        <section className='card bg-[#00272c] w-full max-w-sm mx-auto shadow-2xl border border-white/20 rounded-2xl flex flex-col justify-between items-center gap-y-4 my-auto py-6 px-12'>
+            <div className='flex justify-evenly items-center gap-x-2'>
+                <Image src={"/logo.png"} alt='logo of the company' width={50} height={50} />
+                <h2 className="text-2xl font-bold text-[#e1ff51]">
+                    SkillSphere
+                </h2>
+            </div>
+            <div className='space-y-2 text-center'>
+                <h1 className='text-3xl font-bold text-zinc-50'>Create an Account</h1>
+                <p className='text-md text-zinc-400'>Start your learning journey with us.</p>
+            </div>
+            <fieldset className="fieldset rounded-box w-xs px-4">
+
+                <label className="label">Name</label>
+                <input type="email" className="input bg-[#01343a] text-zinc-50 border-white/20" autoFocus placeholder="Enter Your Name" />
+
+                <label className="label">Email</label>
+                <input type="email" className="input bg-[#01343a] text-zinc-50 border-white/20" placeholder="Enter your Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input bg-[#01343a] text-zinc-50 border-white/20" placeholder="Enter a Password" />
+
+                <label className="label">Confirm Password</label>
+                <input type="password" className="input bg-[#01343a] text-zinc-50 border-white/20" placeholder="Re-enter your Password" />
+
+                <button className="btn bg-[#e1ff51] text-[#00272c] mt-4 font-semibold text-xl">Register</button>
+            </fieldset>
+            <div className="flex items-center gap-2">
+                <hr className="flex-1 border-white/20" />
+                <span className="text-md text-zinc-50">OR</span>
+                <hr className="flex-1 border-white/20" />
+            </div>
+            <div className="flex flex-col gap-4 w-full mb-4">
+                <button className="btn bg-white text-[#00272c] border-[#e5e5e5] w-full">
+                    <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                    Login with Google
+                </button>
+            </div>
+            <p className='font-semibold text-end text-zinc-50'>Already have an account? <Link href={"/login"} className='text-[#e1ff51]'>Login</Link></p>
+        </section>
+    );
+};
+
+export default Registerpage;
